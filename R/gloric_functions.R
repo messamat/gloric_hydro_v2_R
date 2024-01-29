1c73ff6889398cfe49776875421a25f86e92b1db
messamat <messamat@uw.edu>
2024-01-29 12:56:45 -0500

    Deal with issue of changes in station numbers and metadata since 2014...

--- R/gloric_functions.R
+++ R/gloric_functions.R
@@ -1,4 +1,146 @@
 ############################ UTILITY FUNCTIONS ##################################
+#------ readformatGRDC -----------------
+#' Read and pre-format GRDC data
+#'
+#' Reads text file of daily discharge data for a single GRDC station.
+#' Creates columns for year, month, and date of last non-zero flow day +
+#' computes yearly number of days of missing data
+#'
+#' @param path (character) path to the text file of daily discharge data in
+#'   standard GRDC format.
+#'
+#' @return \link[data.table]{data.table} of daily discharge data with additional columns
+#'
+#' @export
+readformatGRDC<- function(path) {
+  #extract GRDC unique ID by formatting path
+  gaugeno <- strsplit(basename(path), '[.]')[[1]][1]
+  
+  #Read GRDC text data
+  gaugetab <- cbind(fread(path, header = T, skip = 40, sep=";",
+                          colClasses = c('character', 'character', 'numeric',
+                                         'numeric', 'integer')),
+                    GRDC_NO = gaugeno)%>%
+    setnames('YYYY-MM-DD', 'dates') %>%
+    setorder(GRDC_NO, dates)
+  
+  #Format data
+  gaugetab[, `:=`(year = as.numeric(substr(dates, 1, 4)), #Create year column
+                  month = as.numeric(substr(dates, 6, 7)), #create month column
+                  integervalue = fifelse(Original == round(Original), 1, 0) #Flag integer discharge values]
+  )]
+  
+  #For each record, compute date of last non-zero flow day
+  gaugetab[, prevflowdate := gaugetab[zero_lomf(Original),'dates', with=F]] %>% #Get previous date with non-zero flow
+    .[Original != 0, prevflowdate:=NA] #If non-zero flow, set prevflowdate to NA
+  
+  #Compute number of missing days per year, excluding NoData values
+  gaugetab[!(Original %in% c(-999, -99, -9999, 99, 999, 9999) | is.na(Original)),
+           `:=`(missingdays = diny(year)-.N,
+                datadays = .N),
+           by= 'year']
+  
+  return(gaugetab)
+}
+
 
 ############################ ANALYSIS FUNCTIONS ##################################
+#------ read_GRDCgauged_paths -----------------
+#' Read file paths to streamflow data from GRDC gauging stations
+#'
+#' Based on selection of gauges, create a list of paths to streamflow data
+#' associated with gauges.
+#'
+#' @param inp_GRDCgaugedir path to directory containing streamflow data GRDC standard files.
+#' @param in_gaugep table containing column named \code{GRDC_NO} with the
+#' gauge IDs that will be used to generate file path.
+#'
+#' @return vector of paths to GRDC-formatted streamflow time series tables, assuming
+#' that files are called "GRDC_NO.txt", GRDC_NO being replaced with a 7-digit integer.
+#'
+#' @export
 
+
+read_GRDCgauged_paths <- function(inp_GRDCgaugedir, in_GRDC_metadata,
+                                  inp_GIRESgaugemetadata, inp_gaugep) { #, gaugeid = 'GRDC_NO' down the line
+  #Read gauges
+  gaugep <- vect(dirname(inp_gaugep), layer=basename(inp_gaugep)) %>%
+    as.data.table
+  
+  #Get data paths of daily records for gauge stations
+  file_names_dt <- gaugep[!is.na(gaugep$grdc_no), 
+                         list(filename = file.path(inp_GRDCgaugedir,
+                                   paste(grdc_no,"_Q_Day.Cmd.txt", sep=""))
+                              ), 
+                         by=grdc_no]
+  
+  #Check those that are missing
+  file_names_dt[, exists := file.exists(filename)]
+  
+  gires_metada <- fread(inp_GIRESgaugemetadata) #get metadata for stations from Messager et al. 2021
+  #the grdc_no of some stations have changed
+  
+  missing_gauges_grdc <- merge(file_names_dt[!(exists),],
+                               in_GRDC_metadata,
+                               by='grdc_no')
+  
+  missing_gauges_gires <- merge(file_names_dt[!(exists),],
+                                gires_metada[, .(GRDC_NO, RIVER, STATION, AREA, LONG_ORG, LAT_ORG)],
+                                by.x='grdc_no',
+                                by.y='GRDC_NO')
+  
+  missing_gauges_gires_rejoin_coord <-  merge(missing_gauges_gires,
+                                              in_GRDC_metadata,
+                                              by.x=c('LONG_ORG', 'LAT_ORG'),
+                                              by.y=c('long', 'lat'),
+                                              suffix=c('_old', '_new'))
+  
+  
+  setnames(missing_gauges_gires, c('RIVER', 'STATION'), c('river', 'station'))
+  
+  
+  missing_gauges_gires_rejoin_name <-  merge(missing_gauges_gires,
+                                             in_GRDC_metadata,
+                                             by=c('river', 'station'),
+                                             suffix=c('_old', '_new')) %>%
+    .[!(grdc_no_new %in% missing_gauges_gires_rejoin_coord$grdc_no_new),]
+  
+  
+  nojoin <- missing_gauges_gires[
+    !(grdc_no %in% c(missing_gauges_gires_rejoin_coord$grdc_no_old,
+                     missing_gauges_gires_rejoin_name$grdc_no_old)),
+  ]
+  
+  missing_gauges_gires_fuzzyjoin <- stringdist_join(nojoin, 
+                                                   in_GRDC_metadata, 
+                                                   mode = "inner",
+                                                   by = c("river", "station"),
+                                                   max_dist = 2) %>%
+    as.data.table %>%
+    .[river.x != 'KURA',] %>%
+    setnames(c('grdc_no.x', 'grdc_no.y'),
+             c('grdc_no_old', 'grdc_no_new'))
+    
+    
+  #Output a clean dataset
+  file_names_dt_clean <- do.call(
+    rbind,
+    list(missing_gauges_gires_rejoin_coord[, .(grdc_no_new, grdc_no_old)],
+         missing_gauges_gires_rejoin_name[, .(grdc_no_new, grdc_no_old)],
+         missing_gauges_gires_fuzzyjoin[, .(grdc_no_new, grdc_no_old)]
+    )
+  ) %>%
+    merge(file_names_dt, ., by.x='grdc_no', by.y='grdc_no_old', all.x=T) %>%
+    .[is.na(grdc_no_new), grdc_no_new := grdc_no] %>%
+    .[, filename := file.path(inp_GRDCgaugedir,
+                             paste(grdc_no_new,"_Q_Day.Cmd.txt", sep=""))
+      ] %>%
+    .[, -'grdc_no', with=F] %>%
+    setnames(c('grdc_no_new'), c('grdc_no'))
+  
+  file_names_dt_clean[, exists := file.exists(filename)]
+  file_names_dt_clean[, .N, by=exists]
+
+  
+  return(file_names_dt_clean)
+}
--- _targets.R
+++ _targets.R
@@ -1,23 +1,58 @@
-source("R/cartoCE_packages.R")
-source("R/cartoCE_functions.R")
+source("R/gloric_packages.R")
+source("R/gloric_functions.R")
 
 rootdir = rprojroot::find_root(has_dir('src'))
 datdir = file.path(rootdir, 'data')
 resdir = file.path(rootdir, 'results')
 
-tar_option_set(format = "qs")
+tar_option_set(format = "qs",
+               controller= crew_controller_local(workers = 4))
 
 #--- Data repositories --------------------------------------------------------
-statsgdb = file.path(resdir, 'env_stats.gdb')
+
 
 #--- Parameters ---------------------------------------------------------------
-excluded_deps = c(75, 92, 93, 94)
+
 
 ############################# Define targets plan ##############################
 list(
-  #------------------------------- Read files ----------------------------------
-  #Read in metadata
-  tar_target(ddt_metadata_path, file.path(datdir, 'metadonnes_cartographie_cours_deau_20231106.xlsx')), #format='file'
-  tar_target(ddt_nets_colnas_path, file.path(resdir, 'cartos_loi_eau_colNAs.csv'), format='file') ,
-
+  #------------------------------- File paths ----------------------------------
+  tar_target(path_GRDCgaugedir, file.path(datdir, "gauges", "grdc", "q_day"), format='file') ,
+  tar_target(path_GRDCmetadata, file.path(datdir, "gauges", "grdc", "GRDC_Stations.xlsx")),
+  tar_target(path_GIRESgaugemetadata, file.path(datdir, 'gauges', 'gires', "high_qual_daily_stations.csv"), format='file'),
+  tar_target(path_gaugep, file.path(resdir, 'stations_preprocess.gdb', 'grdc_p_o20y_cleanjoin'))
+  ,
+  
+  
+  #------------------------------ Read files -----------------------------------
+  tar_target(GRDC_metadata,
+             read_xlsx(path_GRDCmetadata, sheet = 'station_catalogue') %>%
+               as.data.table
+  )
+  # ,
+  # inp_GRDCgaugedir = tar_read(path_GRDCgaugedir)
+  # inp_GIRESgaugemetadata = tar_read(path_GIRESgaugemetadata)
+  # inp_gaugep = tar_read(path_gaugep)
+  # in_GRDC_metadata = tar_read(GRDC_metadata)
+  #
+  # tar_target(GRDCgauged_filenames,
+  #            read_GRDCgauged_paths(
+  #              inp_GRDCgaugedir = path_GRDCgaugedir,
+  #              inp_GRDCmetadata = path_GRDCmetadata,
+  #              inp_GIRESgaugemetadata = path_GIRESgaugemetadata,
+  #              inp_gaugep = path_gaugep
+  #              ))
+  # )
+  # ,
+  # 
+  # GRDCgaugestats = future_map(GRDCgauged_filenames, #To map
+  #                             comp_GRDCdurfreq, #Function to run on each file name
+  #                             maxgap = 20,
+  #                             in_gaugep = gaugep,
+  #                             windowsize = 20,
+  #                             fullwindow = FALSE,
+  #                             monthsel = NULL, #Other arguments in function
+  #                             mdurthresh = 1,
+  #                             verbose = FALSE,
+  #                             .progress = TRUE),
 )
